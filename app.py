50073505543e72eca645b398d0ec3fabd15e8221
Erik Veenstra <zerikveenstraz@gmail.com>
2024-05-14 13:58:33 +0200

    Started working on implementing HTTPS flow.

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from generator_api import GeneratorAPI
+from OpenSSL import SSL
 
 app = Flask(__name__)
+
+CERT_FILE = "/path/to/cert.pem" 
+KEY_FILE = "/path/to/key.pem" 
+
+# Create SSL context 
+context = SSL.Context(SSL.PROTOCOL_TLSv1_2) 
+context.load_cert_chain(CERT_FILE, KEY_FILE) 
 
 api = Api(app)
 api.add_resource(GeneratorAPI, '/generate')
@@ -10,7 +18,7 @@ api.add_resource(GeneratorAPI, '/generate')
 def main():
 
     # TODO:  Alter before deployment
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', ssl_context=context)
 
 if __name__ == "__main__":
     main()
