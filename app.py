9b5dbf03b9657876e949370f9c33ae40a4cfe973
Erik V <zerikveenstraz@gmail.com>
2024-05-31 20:30:26 +0000

    Fixed missing comma

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
 
     # TODO:  Alter before deployment
     # app.run(debug=True, host='0.0.0.0', ssl_context=context)
-    app.run(debug=True, host='0.0.0.0' port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     main()
